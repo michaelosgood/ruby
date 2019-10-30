3bdc0738b7c7c8202d88e1dd1fd1766c80d14563
Michael Osgood <michael.f.osgood@gmail.com>
2019-10-30 16:48:05 -0700

    added operator and comment examples

--- example.rb
+++ example.rb
@@ -20,3 +20,31 @@ puts "Hi there, this is a big
 line break"
 p "Hi there, this is
 a big line break"
+puts
+puts "arithmetic"
+p 1 + 4
+p 10 - 6
+p "+ can be used for" + " contcatenation"
+p 10/5
+p 12/5
+p 12.0/5
+p 12/5.0
+p 12.0/5.0
+
+p "2^4 (two to the power of 4)"
+p "use the double asterisks in ruby"
+p 2 ** 4
+p 5 % 2
+p 6 % 2
+p 10 % 3
+puts
+# single line comments - use a hash tag
+puts "# for single line comments"
+puts "hello world" #this comment will get ignored
+# multi-line
+# example is to use
+# a hash tag after each line
+=begin another ooption is to use =begin and =end
+stuf
+stuff
+=end
