856197d632d4405cf2742096497e4304ae494829
Michael Osgood <michael.f.osgood@gmail.com>
2019-10-30 16:00:07 -0700

    added puts, print, and p method examples

--- example.rb
+++ example.rb
@@ -1,1 +1,22 @@
-puts "Hello world 123"
+puts "Hello world"
+puts "I am alive and well"
+puts "'puts' outputs a string"
+puts 5
+puts "3.24"
+puts 4 + 3
+puts 4 * 3 + 248
+puts "4" + "3"
+puts
+print "works just like 'puts' but doesn't add a line break"
+print "hello"
+print " "
+print "world"
+puts
+puts "the p method"
+puts "Steven Seagal"
+p "Steven Seagal"
+
+puts "Hi there, this is a big
+line break"
+p "Hi there, this is
+a big line break"
