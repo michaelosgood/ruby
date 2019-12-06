79ee10ec828bfce9fb65e51ea7e03fe70a3e5bb6
Michael Osgood <michael.f.osgood@gmail.com>
2019-12-05 17:01:36 -0800

    updated the number & booleans examples

--- number_and_booleans.rb
+++ number_and_booleans.rb
@@ -22,3 +22,38 @@ p 5.class
 
 p 5.234.class
 # outputs Float
+
+p 9999999999999999999999999999999999.class
+
+
+#Convert a string to a number and vice versa
+str = "5"
+p str
+p str.class
+
+# Converts the str string to an integer and then execute the class method -> should output a Integer object
+p str.to_i
+
+# Converts the str string to a float
+p str.to_f
+p str.to_f.class
+
+# Converts the number to a string
+number = 10
+p number.class
+p number.to_s.class
+
+# Converts an int to a Float
+p number.to_f
+p number.to_f.class
+
+
+# Converting a float to an integer
+pi = 3.14
+p pi.to_i
+
+# Converting a float to a string
+p pi.to_s
+
+# For converting values to the same value (i.e. string to a string) ruby returns a string
+p "3".to_s.class
