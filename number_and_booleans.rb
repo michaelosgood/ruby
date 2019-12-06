9138cbcfcdf7216483eba7b9ffad83eb9df2d109
Michael Osgood <michael.f.osgood@gmail.com>
2019-12-05 16:49:19 -0800

    added intro to numbers and booleans file

---
+++ number_and_booleans.rb
@@ -0,0 +1,24 @@
+# Intro to Numbers
+# Two types of number
+# 1) Integers = whole Numbers
+p 5
+p 0
+
+# 2) Floating point Numbers
+p 5.987
+p 2.71
+p -10.23
+
+
+# .next method
+# adds 1 to the value
+next_var = 5.next
+puts next_var
+#outputs 6 as the value
+
+# the class method will tell you what class method created the object
+p 5.class
+# outputs Integer
+
+p 5.234.class
+# outputs Float
