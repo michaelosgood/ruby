fec6ef7d270f2b2cd906fd4ba0ef4998252deedb
Michael Osgood <michael.f.osgood@gmail.com>
2020-01-14 16:38:27 -0800

    updaed float methods

---
+++ float_methods.rb
@@ -0,0 +1,18 @@
+# Floor Method - rounds down to the closest integer (returns 10)
+p 10.5.floor
+puts
+
+# Ceiling Method - rounds up the the closest integer (returns 11)
+p 10.5.ceil
+puts
+
+# Rounding Method (rounds to 4)
+p 3.6.round
+
+#rounds to the 4th decimal point
+p 3.14159.round(4)
+puts
+
+# Absolute methods (returns absolute value both returns 50)
+p -150.abs 
+p 150.abs
